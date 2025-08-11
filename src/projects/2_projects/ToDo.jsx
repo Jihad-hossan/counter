7f70a78c7d3f8c3532121d102869f254a4f830c0
Jihad-hossan <jihad.marine@gmail.com>
2025-08-11 12:35:56 +0600

    fix the errors and followed React way to get and changed the input field

--- src/projects/2_projects/ToDo.jsx
+++ src/projects/2_projects/ToDo.jsx
@@ -4,50 +4,37 @@ import todoData from "./todoData";
 const ToDo = () => {
 
     const [data, setData] = useState(todoData);
+    const [inputValue, setInputValue] = useState("");
 
-    const removeTodo = (id) => {
-        const newData = data.filter((element, index)=>{
-            return element.id !== id;
+    const removeItem = (id) => {
+        const newData = data.filter((ele, index)=>{
+            return id !== ele.id;
         });
 
-        const bringBack = document.querySelector(".bring-back");
-        bringBack.classList.remove("d-none");
-
         setData(newData);
-
     }
+
     const removeAll = () => {
         setData([]);
-
-        const bringBack = document.querySelector(".bring-back");
-        bringBack.classList.remove("d-none");
-
     }
 
-    const bringBackAll = () => {
-        setData(todoData);
-        const bringBack = document.querySelector(".bring-back");
-        bringBack.classList.add("d-none");
-    }
-
-    const addTask = () => {
-        const input = document.querySelector("input");
-        if (input.value.trim() === "") {
-            alert("Please enter a task.");
+    const addItem = () => {
+        if(inputValue.trim() === ''){
             return;
         }
+        const newItem = {
+            id: Date.now() + Math.random(),
+            title: inputValue,
+            completed: true
+        }
 
-        const newTask = {
-            id: data.length + 1,
-            title: input.value,
-            completed: false
-        };
+        setData((prev)=>{
+            return [newItem, ...prev];
+        });
 
-        setData(prevData => [...prevData, newTask]);
-        input.value = ""; // Clear the input field after adding the task
+        setInputValue("");
     }
-
-
+ 
     return (
         <div className="container py-5">
             <div className="row">
@@ -55,24 +42,23 @@ const ToDo = () => {
             </div>
             <div className="row">
                 <div className="add-item d-flex flex-column mb-3 gap-2">
-                    <input className="form-control" type="text" />
-                    <button onClick={addTask} className="btn btn-primary">Add Task</button>
+                    <input onChange={(e)=>{
+                        setInputValue(e.target.value);
+                    }} value={inputValue} className="form-control" type="text" />
+                    <button onClick={addItem}  className="btn btn-primary">Add Task</button>
                 </div>
                 <div className="list d-flex gap-3 flex-column">
                     {data.map((item, index)=>{
                         return (
                             <div className="d-flex p-3 card justify-content-between flex-row align-items-center" key={item.id} data-id={item.id}>
                                 <h4 className="list-title">{item.title}</h4>
-                                <button onClick={()=>{
-                                    return removeTodo(item.id);
-                                }} className="btn btn-danger">Delete</button>
+                                <button onClick={()=> {
+                                    return removeItem(item.id);
+                                }}  className="btn btn-danger">Delete</button>
                             </div>
                         )
                     })}
-                </div>
-                <div className="d-flex gap-2 align-items-center mt-4 justify-content-center">
-                    <button onClick={removeAll} className="btn btn-danger">Remove All</button>
-                    <button onClick={bringBackAll} className="btn btn-primary bring-back d-none">Bring Back All</button>
+                    <button  onClick={removeAll} className="btn btn-danger">Delete All</button>
                 </div>
             </div>
         </div>
